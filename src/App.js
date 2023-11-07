f01e14eed6bcc3fa5a193b18a4d34f0b7b9ae7c8
Fathir achmad sabiilah <fathir17.fa@gmail.com>
2023-11-07 18:46:23 +0700

    Add logo(2)

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ const router = createBrowserRouter([
       { path: "/projects", element: <ProjectsPageView /> },
     ]
   },
-  // { path: "/register", element: <RegisterPage/> }
 ])
 
 function App() {
