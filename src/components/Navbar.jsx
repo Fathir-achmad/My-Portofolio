53f1cebb337bc8dae105754c5ac469d511e8ba84
Fathir achmad sabiilah <fathir17.fa@gmail.com>
2023-11-09 11:29:42 +0700

    Fixing color navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Flex, Text, Link, Box, useMediaQuery } from "@chakra-ui/react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { DrawerNavbar } from "./Drawer/DrawerNavbar";
 
 export const Navbar = () => {
   const navigate = useNavigate();
   const [isMediumScreen] = useMediaQuery("(max-width: 768px)");
+  const { pathname } = useLocation();
+
+  // Menghapus host dan protokol dari URL saat ini
+  const currentPath = pathname.replace(window.location.origin, "");
+
   const handleClick = (path) => {
     navigate(path);
   };
@@ -28,19 +33,54 @@ export const Navbar = () => {
           flexWrap="wrap"
         >
           <Text fontSize="2xl" fontWeight="bold">
-            <Link onClick={() => handleClick("/about")}>About</Link>
+            <Link
+              onClick={() => handleClick("/about")}
+              style={{
+                color: currentPath === "/about" ? "blue" : "white"
+              }}
+            >
+              About
+            </Link>
           </Text>
           <Text fontSize="2xl" fontWeight="bold">
-            <Link onClick={() => handleClick("/education")}>Education</Link>
+            <Link
+              onClick={() => handleClick("/education")}
+              style={{
+                color: currentPath === "/education" ? "blue" : "white"
+              }}
+            >
+              Education
+            </Link>
           </Text>
           <Text fontSize="2xl" fontWeight="bold">
-            <Link onClick={() => handleClick("/skills")}>Skills</Link>
+            <Link
+              onClick={() => handleClick("/skills")}
+              style={{
+                color: currentPath === "/skills" ? "blue" : "white"
+              }}
+            >
+              Skills
+            </Link>
           </Text>
           <Text fontSize="2xl" fontWeight="bold">
-            <Link onClick={() => handleClick("/work")}>Work</Link>
+            <Link
+              onClick={() => handleClick("/work")}
+              style={{
+                color: currentPath === "/work" ? "blue" : "white"
+              }}
+            >
+              Work
+            </Link>
           </Text>
           <Text fontSize="2xl" fontWeight="bold">
-            <Link onClick={() => handleClick("/projects")}>Projects</Link>
+            <Link
+              onClick={() => handleClick("/projects")}
+              style={{
+                color: currentPath === "/projects" ? "blue" : "white"
+              }}
+            >
+              Projects
+            </Link>
           </Text>
         </Flex>
       )}
